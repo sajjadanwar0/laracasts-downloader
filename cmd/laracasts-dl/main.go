80c7b2b0f4ae528ef9698ea33891b3e4121c4666
sajjadanwar0 <sajjadanwar400@gmail.com>
2025-02-10 18:59:47 +0000

    Initial commit

--- cmd/laracasts-dl/main.go
+++ cmd/laracasts-dl/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/sajjadanwar0/laracasts-dl/internal/downloader"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -18,9 +19,36 @@ func main() {
 	// Parse flags
 	flag.Parse()
 
-	// Load environment variables
-	if err := godotenv.Load(); err != nil {
-		fmt.Printf("Error loading .env file: %v\n", err)
+	// Get the executable directory
+	ex, err := os.Executable()
+	if err != nil {
+		fmt.Printf("Error getting executable path: %v\n", err)
+		os.Exit(1)
+	}
+	exPath := filepath.Dir(ex)
+
+	// Try to load .env from different possible locations
+	envPaths := []string{
+		".env",                              // Current directory
+		"../../.env",                        // Two levels up (from cmd/laracasts-dl to project root)
+		filepath.Join(exPath, ".env"),       // Executable directory
+		filepath.Join(exPath, "../../.env"), // Two levels up from executable
+	}
+
+	envLoaded := false
+	for _, path := range envPaths {
+		if err := godotenv.Load(path); err == nil {
+			envLoaded = true
+			break
+		}
+	}
+
+	if !envLoaded {
+		fmt.Println("Error: Could not find .env file in any of these locations:")
+		for _, path := range envPaths {
+			absPath, _ := filepath.Abs(path)
+			fmt.Printf("- %s\n", absPath)
+		}
 		os.Exit(1)
 	}
 
@@ -32,12 +60,11 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Rest of your main function...
 	// Check if any content type flag was specified
 	seriesSpecified := isFlagSpecified("s")
-	larabitSpecified := isFlagSpecified("l")
-	topicSpecified := isFlagSpecified("t")
 
-	if !seriesSpecified && !larabitSpecified && !topicSpecified {
+	if !seriesSpecified {
 		fmt.Println("Please specify at least one content type to download:")
 		fmt.Println("  -s [slug]     : Download series (optional: specific series slug)")
 		os.Exit(1)
@@ -57,17 +84,18 @@ func main() {
 	}
 
 	// Process series downloads
-	if seriesSpecified {
-		if seriesFlag != "" {
-			if err := dl.DownloadSeries(seriesFlag); err != nil {
-				fmt.Printf("Error downloading series: %v\n", err)
-			}
-		} else {
-			if err := dl.DownloadAllSeries(); err != nil {
-				fmt.Printf("Error downloading all series: %v\n", err)
-			}
+	//if seriesSpecified {
+	if seriesFlag != "" {
+		if err := dl.DownloadSeries(seriesFlag); err != nil {
+			fmt.Printf("Error downloading series: %v\n", err)
 		}
 	}
+	//else {
+	//if err := dl.DownloadAllSeries(); err != nil {
+	//fmt.Printf("Error downloading all series: %v\n", err)
+	//}
+	//}
+	//}
 }
 
 // isFlagSpecified checks if a flag was specified on the command line
